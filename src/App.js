b4359a5bf65aefa0788c9eb9f131010357f0d254
prabin738 <prabinsingh750@gmail.com>
2025-06-08 16:43:15 +0545

    Home search nav bar created or copied from bootstrap

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import "./App.css";
 
-let name = "Prabin";
 function App() {
   return (
     <>
-      <div className="blank">
-        <nav>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact US</li>
-        </nav>
-      </div>
-      <div className="container">
-        <h1>Hello {name}</h1>
-        <p>
-          Bootstrap is a free, open-source CSS framework for web development,
-          used to create responsive, mobile-first websites and web applications.
-          It provides a collection of pre-designed templates, CSS styles, and
-          JavaScript components that help developers efficiently and effectively
-          create visually appealing and consistent interfaces.{" "}
-        </p>
-      </div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            Text Utlies
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/">
+                  About US
+                </a>
+              </li>
+            </ul>
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
+        </div>
+      </nav>
     </>
   );
 }
